3e5d31f7a41abc1f87d371ab29f7e4fe0b6914c8
Dstrange1234x <74235077+Dstrange1234x@users.noreply.github.com>
2021-01-25 16:56:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var database;
+var gameState=0;
+var playerCount;
+var player;
+var form;
+var game;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+
+    game=new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
